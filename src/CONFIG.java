c45ed788c6dcc9d634ef725f44a1fe6a3b3a3d0d
traceyomphile <omphiletracey@gmail.com>
2026-06-29 13:12:00 +0200

    Ignored secret files

--- src/CONFIG.java
+++
@@ -1,6 +0,0 @@
-
-public class CONFIG {
-    public static final String URL_STRING = "jdbc:mysql://localhost:3306/onpitch";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "Tayleersa1201$";
-}
